bd441db36c716f2cd9e06262cb2e8f65c0cb2f3c
str-username <nikolay.semenov91@gmail.com>
2023-03-09 13:55:59 +0300

    style: formatter

--- 01-terraform/01-modules/01-aws/rds/main.tf
+++ 01-terraform/01-modules/01-aws/rds/main.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "this" {
   description = "Allow access to rds cluster from ${var.allowed_access_from_name}"
   name        = "${var.cluster_name}-ingress-allowed"
   vpc_id      = var.vpc_id
-  tags        = merge({ "Name" = "${var.cluster_name}-ingress-allowed"}, var.tags)
+  tags        = merge({ "Name" = "${var.cluster_name}-ingress-allowed" }, var.tags)
 
   ingress {
     from_port   = var.port
